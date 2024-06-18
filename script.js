7c67af8b475b5bfa6bddcfacbed04962bfb81f9d
“Suyeon6775” <163228437+Suyeon6775@users.noreply.github.com>
2024-06-18 22:52:34 +0900

    dd

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+document.addEventListener('mousemove', (event) => {
+  const circles = document.querySelectorAll('.floating');
+  const mouseX = event.clientX;
+  const mouseY = event.clientY;
 
+  circles.forEach((circle, index) => {
+    const delay = index * 100; // 각 원이 약간의 지연을 갖도록 설정 (100ms 단위)
+    setTimeout(() => {
+      circle.style.transform = `translate(${mouseX}px, ${mouseY}px)`;
+    }, delay);
+  });
+});
