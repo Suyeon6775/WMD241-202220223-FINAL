58fa759a5d15270d4fabb72feda312e6b5f2aeb9
“Suyeon6775” <163228437+Suyeon6775@users.noreply.github.com>
2024-06-18 21:41:34 +0900

    ss

--- script.js
+++ script.js
@@ -1,19 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const modal = document.getElementById('modal');
-  const modalImg = document.getElementById('modal-img');
-  const captionText = document.getElementById('caption');
-  const galleryItems = document.querySelectorAll('.gallery-item');
-  const span = document.getElementsByClassName('close')[0];
 
-  galleryItems.forEach((item) => {
-    item.addEventListener('click', () => {
-      modal.style.display = 'block';
-      modalImg.src = item.src;
-      captionText.innerHTML = item.getAttribute('data-description');
-    });
-  });
-
-  span.onclick = function () {
-    modal.style.display = 'none';
-  };
-});
